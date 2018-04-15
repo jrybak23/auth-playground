853247f9f61b69c414951234b98f258e877ec5b5
jrybak2312 <rybakigor1996@gmail.com>
2018-04-15 19:54:59 +0300

    fixed security config

--- http-basic-playground-service/src/main/java/com/example/httpbasicplayground/config/SecurityConfig.java
+++ http-basic-playground-service/src/main/java/com/example/httpbasicplayground/config/SecurityConfig.java
@@ -5,11 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.Collections;
 
@@ -19,6 +19,7 @@ import java.util.Collections;
  */
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -28,8 +29,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/profile").authenticated()
                 .antMatchers("/").permitAll()
                 .and()
-                .logout().clearAuthentication(true)
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
     @Override
